a420ea9380c9a8d6671a79f8870a8f5e2f9e20c7
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-04-30 16:41:37 +0300

    Yıl Gün Ay hesaplama

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Gün_saat_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4gun = new System.Windows.Forms.Label();
+            this.label5saat = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8dakika = new System.Windows.Forms.Label();
+            this.label10saniye = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5Ay = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.Maroon;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.Menu;
+            this.dateTimePicker1.Location = new System.Drawing.Point(82, 18);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(302, 29);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(82, 67);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(302, 29);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-2, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1.Gün :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(-2, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2.Gün :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(151, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Zaman Fark Hesapla ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(31, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Gün  : ";
+            // 
+            // label4gun
+            // 
+            this.label4gun.AutoSize = true;
+            this.label4gun.BackColor = System.Drawing.Color.Transparent;
+            this.label4gun.ForeColor = System.Drawing.Color.White;
+            this.label4gun.Location = new System.Drawing.Point(98, 212);
+            this.label4gun.Name = "label4gun";
+            this.label4gun.Size = new System.Drawing.Size(32, 24);
+            this.label4gun.TabIndex = 6;
+            this.label4gun.Text = "00";
+            // 
+            // label5saat
+            // 
+            this.label5saat.AutoSize = true;
+            this.label5saat.BackColor = System.Drawing.Color.Transparent;
+            this.label5saat.ForeColor = System.Drawing.Color.White;
+            this.label5saat.Location = new System.Drawing.Point(98, 288);
+            this.label5saat.Name = "label5saat";
+            this.label5saat.Size = new System.Drawing.Size(32, 24);
+            this.label5saat.TabIndex = 8;
+            this.label5saat.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(30, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Saat  : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(9, 321);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Dakika  : ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(7, 362);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 24);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Saniye  : ";
+            // 
+            // label8dakika
+            // 
+            this.label8dakika.AutoSize = true;
+            this.label8dakika.BackColor = System.Drawing.Color.Transparent;
+            this.label8dakika.ForeColor = System.Drawing.Color.White;
+            this.label8dakika.Location = new System.Drawing.Point(98, 326);
+            this.label8dakika.Name = "label8dakika";
+            this.label8dakika.Size = new System.Drawing.Size(32, 24);
+            this.label8dakika.TabIndex = 8;
+            this.label8dakika.Text = "00";
+            // 
+            // label10saniye
+            // 
+            this.label10saniye.AutoSize = true;
+            this.label10saniye.BackColor = System.Drawing.Color.Transparent;
+            this.label10saniye.ForeColor = System.Drawing.Color.White;
+            this.label10saniye.Location = new System.Drawing.Point(98, 364);
+            this.label10saniye.Name = "label10saniye";
+            this.label10saniye.Size = new System.Drawing.Size(32, 24);
+            this.label10saniye.TabIndex = 8;
+            this.label10saniye.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(52, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ay : ";
+            // 
+            // label5Ay
+            // 
+            this.label5Ay.AutoSize = true;
+            this.label5Ay.BackColor = System.Drawing.Color.Transparent;
+            this.label5Ay.ForeColor = System.Drawing.Color.White;
+            this.label5Ay.Location = new System.Drawing.Point(98, 250);
+            this.label5Ay.Name = "label5Ay";
+            this.label5Ay.Size = new System.Drawing.Size(32, 24);
+            this.label5Ay.TabIndex = 10;
+            this.label5Ay.Text = "00";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(486, 415);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(65, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Nihat Beyi";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(563, 437);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label5Ay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10saniye);
+            this.Controls.Add(this.label8dakika);
+            this.Controls.Add(this.label5saat);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4gun);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalan Gün Saat Hesaplama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4gun;
+        private System.Windows.Forms.Label label5saat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8dakika;
+        private System.Windows.Forms.Label label10saniye;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5Ay;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gün_saat_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime gun1 = dateTimePicker1.Value;
+            DateTime gun2 = dateTimePicker2.Value;
+
+
+            System.TimeSpan zaman;
+            zaman = gun2.Subtract(gun1);
+
+            //gün yazdırıyoruz
+            int gun = Convert.ToInt32(zaman.TotalDays);
+            label4gun.Text = gun.ToString("N") + "  " + "Gün Nefes alıyorsun   ";
+
+            //ay yazdırıyorsun
+            int ay = Convert.ToInt32(zaman.TotalDays) / 30;
+            label5Ay.Text = ay.ToString("N") + "  " + " Ay Nefes Alıyorsun ";
+
+            //saat yazdırıyoruz
+            int saat = Convert.ToInt32(zaman.TotalHours);
+            label5saat.Text = saat.ToString("N") + "  " + "Saat Nefes Alıyorsun   ";
+
+            //dakika yazdıyoruz
+            int dakika = Convert.ToInt32(zaman.TotalMinutes);
+            label8dakika.Text = dakika.ToString("N") + "  " + "Dakika Nefes Alıyorsun  ";
+
+            //saniye yazdıryoruz
+            int saniye = Convert.ToInt32(zaman.TotalSeconds);
+            label10saniye.Text = saniye.ToString("N") + "  " + "Saniye Nefes Alıyorsun   ";
+
+
+
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Red;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/n.beyi");
+        }
+    }
+}
